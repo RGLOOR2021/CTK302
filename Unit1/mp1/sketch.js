f599567637c1587170883a50cad36ae02feea4a0
RPGLOOR <rochelegloor@gmail.com>
2021-09-11 13:16:25 -0500

    MP1 2

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,10 +1,26 @@
 function setup() {
   createCanvas(500, 500);
-}
+  rectMode(CENTER);
+  ellipseMode(CENTER);
 
-function draw()
-fill(110);
-ellipse(100,100,100,100);
-{
 
 }
+
+function draw() {
+background(200, 52, 155);
+
+// this shows mouse location - comment it out when you're done!
+fill(230, 52, 185);
+text("mouseX + ", " + mouseY, 40, 40");
+}
+
+
+  if (mouseIsPressed) {
+    // stuff that shows when the mouse is pressed
+    ellipse(500, 400, 500, 500);
+
+  } else {
+    // when the mouse isn't pressed!
+    triangle(200, 700, 500, 200, 800, 700);
+
+  }
