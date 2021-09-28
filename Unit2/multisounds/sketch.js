86110e7bb08ab33ca811c93b47ecf9e3bf6a5cd6
RPGLOOR <rochelegloor@gmail.com>
2021-09-28 09:15:11 -0500

    multisounds button

---
+++ Unit2/multisounds/sketch.js
@@ -0,0 +1,71 @@
+let state = 0;
+let song1, song2, song3;
+
+function preload() {
+  song1 = loadSound("assets/donkey.mp3");
+  song2 = loadSound("assets/skate.mp3");
+  song3 = loadSound("assets/thrill.mp3");
+
+  song1.play();
+  song2.play();
+  song3.play();
+  song1.pause();
+  song2.pause();
+  song3.pause();
+  //spanning a song in a loop don't add in the draw function
+}
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  switch(state){
+    case 0:
+    //spawn song 1
+    backgroud('red');
+    song1.play();
+    state = 1
+    break;
+
+    case 1:
+    //listen song 1
+    background('yellow');
+    break;
+
+    case 2:
+    //spawn song 2
+    song2.play();
+    state = 3;
+    break;
+
+    case 3:
+    //listen song 2
+    backgroud('green');
+    break;
+
+    case 4:
+    //spawn song 3
+    song3.play();
+    state = 5;
+    break;
+
+    case 5:
+    //listen to song3
+    background ('grey');
+    break;
+
+
+
+  }
+
+}
+
+function mouseReleased() {
+
+  state++;
+  if (state > 5) {
+    state = 0;
+  }
+
+}
