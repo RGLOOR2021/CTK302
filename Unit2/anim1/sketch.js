0e86cdc595c0d60f616d86137be818f73120ffbc
RPGLOOR <rochelegloor@gmail.com>
2021-09-29 21:19:50 -0500

    anim1 new

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -2,17 +2,24 @@ let x = 0
 
 function setup() {
   createCanvas(500, 500);
+  noStroke();
+
 }
 
-function draw(){
-background("grey");
-rect(x,100,100,100);
+function draw() {
+  background("white");
+  fill("red");
+  rect(x, 100, 150, 150);
+  fill("black");
+  ellipse(x, 250, 200, 200);
+  fill("grey");
+  rect(x, 200, 300, 10);
 
 
-x = x + 1;
-if (x >= width) {
-x = 0;
-}
+  x = x + 1;
+  if (x >= width) {
+    x = 0;
+  }
 
 
 }
