53259005af0c712ab3d3d099212e57543fe04df6
RPGLOOR <rochelegloor@gmail.com>
2021-10-07 21:59:16 -0500

    multisounds

--- Unit2/multisounds/sketch.js
+++ Unit2/multisounds/sketch.js
@@ -3,9 +3,9 @@ let song1, song2, song3;
 let img1, img2, img3;
 
 function preload() {
-  song1 = loadSound("assets/truands.mp4");
-  song2 = loadSound("assets/etoile.mp4");
-  song3 = loadSound("assets/cri.mp4");
+  song1 = loadSound("assets/sot.mp3");
+  song2 = loadSound("assets/dress.mp3");
+  song3 = loadSound("assets/prs.mp3");
   img1 = loadImage("assets/zw3.jpg");
   img2 = loadImage("assets/zw2.jpg");
   img3 = loadImage("assets/zw1.jpg");
@@ -22,6 +22,7 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
+  background(255, 0, 0);
 }
 
 function draw() {
@@ -35,30 +36,52 @@ function draw() {
       break;
 
     case 1:
+      break;
+
+
+    case 2:
       //listen song 1
       background('yellow');
       image(img2, 100, 01, 400, 500);
       song2.play();
-      state = 2
+      state = 3
       break;
 
-    case 2:
+    case 3:
+      break;
+
+
+    case 4:
       //spawn song 2
       background('red');
       image(img3, 150, 01, 400, 500);
       song3.play();
-      state = 0;
+      state = 5;
       break;
+
+    case 5:
+      break;
+
+    case 6:
+      background("black");
+      state = 0
 
   }
 
 }
 
 function mouseReleased() {
+  song1.pause();
+  song2.pause();
+  song3.pause();
 
   state++;
-  if (state > 3) {
+  if (state > 6) {
     state = 0;
   }
 
+}
+
+function touchStarted() {
+  getAudioContext().resume();
 }
