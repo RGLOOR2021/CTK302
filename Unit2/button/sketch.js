662bfe2413adfd117ae973efccc595dc5f6f147d
RPGLOOR <rochelegloor@gmail.com>
2021-10-07 22:51:05 -0500

    button

--- Unit2/button/sketch.js
+++ Unit2/button/sketch.js
@@ -1,34 +1,41 @@
-let state = 0 ;
+let state = 0;
 
 function setup() {
   createCanvas(500, 500);
+  textSize(100);
+  textAlign(CENTER);
 }
 
 function draw() {
 
   switch (state) {
-      case 0:
+    case 0:
       background('white');
-      text("case 0", 250, 250);
+      text("FUTURE", 250, 250);
       break;
-       case 1:
-      background('grey');
-      text("case 1", 250, 250);
+    case 1:
+      background('white');
+      text("IS", 250, 250);
       break;
-  case 2:
-      background('pink');
-      text("case 2", 250, 250);
+    case 2:
+      background('black');
+      fill("white");
+      text("Z\nEROWAST\nE", 250, 250);
       break;
   }
-fill('purple');
-rect(100,100,100,100);
+  fill('black');
+  stroke("white");
+  rect(width / 2 - 50, height - 100, 100, 50);
+
 
 }
 
 function mouseReleased() {
-  if ((mouseX > 100) && (mouseX < 200) && (mouseY > 100) && (mouseY < 200));
-  state++ ;
-  if (state > 2) {
-    state = 0 ;
+  if ((mouseX > width / 2 - 50) && (mouseX < width / 2 + 50) && (mouseY > height - 100) && (mouseY < height - 50)) {
+    state++;
+
+    if (state > 2) {
+      state = 0;
+    }
   }
 }
