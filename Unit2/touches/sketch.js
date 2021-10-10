d4958271c3009646291dd41215293804f60d032e
RPGLOOR <rochelegloor@gmail.com>
2021-10-09 20:21:29 -0500

    touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,32 +1,41 @@
 let numberOfTouches ;
+let img1, img2, img3;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(250, 400);
+  img1 = loadImage("assets/1.png")
+  img2 = loadImage("assets/2.png")
+  img3 = loadImage("assets/3.png")
 }
 
 function draw() {
   clear();
+  background("pink");
+  fill("white");
   numberOfTouches = touches.length;
   text(numberOfTouches + ' touches', 5, 10);
 
   switch(numberOfTouches) {
     case 0:
-      text("no one is touching the screen", 5, 22) ;
+      text("if you touch here you get a prize", 10, 50) ;
       break ;
 
       case 1:
-       text("it's kind of lonely here", 5, 22) ;
-      // put a picture here
+       text("I didn't feel that", 10, 50 ) ;
+       image(1, 10, 10, 240, 400);
+
       break ;
 
       case 2:
-      text("two fingers are touching the screen", 5, 22) ;
-            // put a picture here
+      text("hmmm not yet", 10, 50 ) ;
+      image(1, 10, 10, 240, 400);
+
       break ;
 
       case 3:
-     text("three are touching the screen", 5, 22) ;
-            // put a picture here
+     text("There you go!", 10, 50) ;
+     image(1, 10, 10, 240, 400);
+
       break ;
 
 
