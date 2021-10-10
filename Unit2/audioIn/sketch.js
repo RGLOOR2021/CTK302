ed670abeb10b0ed48a2b3216f11d870c0bce6b51
RPGLOOR <rochelegloor@gmail.com>
2021-10-09 20:54:38 -0500

    audioIn

--- Unit2/audioIn/sketch.js
+++ Unit2/audioIn/sketch.js
@@ -1,13 +1,12 @@
-var mic;
-var vol;
-var c = 'green';
+let mic;
+let vol;
+let c = 'pink';
 let x = 0 ;
 
 function setup() {
   createCanvas(400, 400);
 
-  // code for initializing mic in.
-  mic = new p5.AudioIn(); // what does "new" mean?
+  mic = new p5.AudioIn();
   mic.start();
 }
 
@@ -15,30 +14,21 @@ function setup() {
 function draw() {
   background(c);
 
-  // get the sound input
-  vol = (mic.getLevel().toFixed(2)); // returned level is between 0 and 1
+  vol = (mic.getLevel().toFixed(2));
 
-  // check how loud the input is
-  if (vol > .20) { // if the volume is LOUD?
-    // do something
+  if (vol > .20) {
 
-    c = color(random(255), random(255), random(255)); // here I'm setting the background to a random color
+    c = color(random(120), random(30), random(235));
   }
 
-  // extra stuff for debugging
-  textSize(18);
+  textSize(20);
   text("Click the screen first to give\npermission for mic input.\nMy volume is " + vol, 10, 60);
 
-  //we can add images
   rect(vol * 100, 100, 50, 50);
-
 //   x = map(vol, 0, .2, 0, width) ;
 //   rect(x, 200, 50, 50);
 
 }
-
-// you need this code for audio programs and also, the user
-// needs to click the screen before the mic input will work.
 
 function touchStarted() {
   getAudioContext().resume();
