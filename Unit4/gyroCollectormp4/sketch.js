1555f30284e9b9e5be4905daaa3afc520a3c371a
RPGLOOR <rochelegloor@gmail.com>
2021-11-20 23:54:55 -0600

    gyro

--- Unit4/gyroCollectormp4/sketch.js
+++ Unit4/gyroCollectormp4/sketch.js
@@ -1,13 +1,13 @@
-// variables needed for gyroscope
-var beta, gamma; // orientation data
-var x = 0; // acceleration data
+
+var beta, gamma;
+var x = 0;
 var y = 0;
 var z = 0;
 var xPosition = 0;
 var yPosition = 0;
 let img1, img2;
+let font;
 
-// var bunnyImage;
 var cars = [];
 var frogPos;
 
@@ -18,23 +18,18 @@ function setup() {
 
   img1 = loadImage("assets/jt1.jpg");
   img2 = loadImage("assets/handsole.png");
+  font = loadFont( "assets/akira.otf")
 
-  // initialize accelerometer variables
   alpha = 0;
   beta = 0;
   gamma = 0;
 
-
-  // spawn a bunch of cars
   for (var i = 0; i < 40; i++) {
     cars.push(new Car());
   }
 
-  // initialize the frog's position
   frogPos = createVector(width / 2, height - 80);
 
-  // load any images you need
-  //bunnyImage = loadImage("assets/bunny.jpg");
   imageMode(CENTER);
   rectMode(CENTER);
   noStroke();
@@ -42,35 +37,25 @@ function setup() {
 
 function draw() {
 
-  //background("black"); // light blue
-  //image(img1, 1, 1, windowWidth, windowHeight);
-  image(img1, 1, 1, 3, 3);
+  background("pink");
+  textFont(font);
 
 
-  // the map command !!!!
-  // takes your variable and maps it from range 1 to range 2
-  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
   xPosition = map(gamma, -18, 18, 0, width);
   yPosition = map(beta, 25, 45, 0, height);
 
+//FROG
+  push();
+  translate(xPosition, yPosition);
 
-  // move the frog around the screen
-  push(); // before you use translate, rotate, or scale commands, push and then pop after
-  translate(xPosition, yPosition); // move everything over by x, y
-  //  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
-
-  // draw the FROG
-  //image(img1, 0, 0, 500, 500);
-  //fill('green');
-  //ellipse(0, 0, 80, 80);
+  fill('green');
+  ellipse(100, 100, 180, 180);
   pop();
 
-
-  // update the frog's position using the accelerometer data
   frogPos.x = xPosition;
   frogPos.y = yPosition;
 
-  // iterate through the car loop to move them and see if we need to delete cars
+
   for (var i = 0; i < cars.length; i++) {
     cars[i].display();
     cars[i].drive();
@@ -79,68 +64,55 @@ function draw() {
     }
   }
 
-  // MORE DECORATIONS - write that pretty ATK type on top.
-  //fill('white');
-  //textSize(40);
-  //textAlign(CENTER);
-  //text("your words or image here", width / 2, 600, windowWidth - 200, windowHeight - 200);
+
+  fill('white');
+  textSize(40);
+  textAlign(CENTER);
+  text("stop\n \nlook to the sky\n \ntake a deep breath", width / 2, 600, windowWidth - 200, windowHeight - 200);
 
 
-  // Debugging information -- take this out when you're ready for production!
-  // Just a bunch of text commands to display data coming in from addEventListeners
-  textAlign(LEFT);
+
+  textAlign(RIGHT);
   textSize(20);
   fill('black');
-  text("orientation data:", 25, 25);
+  text("orientation data:", 475, 25);
   textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
+  text("alpha: " + alpha, 500, 50);
+  text("beta: " + beta, 498, 70);
+  text("gamma: " + gamma, 500, 90);
   textSize(20);
-  text("acceleration data:", 25, 125);
+  text("acceleration data:", 500, 125);
   textSize(15);
-  text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y, 25, 170);
-  text("z = " + z, 25, 190);
-  //it can be comment production
+  text("x = " + x, 525, 150);
+  text("y = " + y, 525, 170);
+  text("z = " + z, 525, 190);
 
 
 }
 
 function deviceShaken() {
-  // re-spawn cars
-  cars = []; // clear the array first
+  cars = [];
   for (var i = 0; i < 40; i++) {
     cars.push(new Car());
   }
 }
 
 
-// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
-
-// Read in accelerometer data
 window.addEventListener('deviceorientation', function(e) {
   alpha = e.alpha;
   beta = e.beta;
   gamma = e.gamma;
 });
 
-
-// accelerometer Data
 window.addEventListener('devicemotion', function(e) {
-  // get accelerometer values
+
   x = e.acceleration.x;
   y = e.acceleration.y;
   z = e.acceleration.z;
 });
 
-
-
-
-
-// car class!!
 function Car() {
-  // attributes
+
   this.pos = createVector(100, 100);
   this.vel = createVector(random(-5, 5), random(-5, 5))
   if (random(2) > 1) {
@@ -148,24 +120,11 @@ function Car() {
   } else {
     this.image = img2;
   }
-  //this.r = random(255);
-  //this.g = random(255);
-  //this.b = random(255);
-  //this.a = random(255);  // alpha opacity value for fill!
 
 
-  // methods
-  this.display = function() { //needs to be changed
+  this.display = function() {
 
-    // maybe use an image here instead!
-    //fill(this.r, this.g, this.b, this.a);
-    //ellipse(this.pos.x - 50, this.pos.y, 50, 50);
-    //ellipse(this.pos.x + 50, this.pos.y, 50, 50);
-    //rect(this.pos.x + 17, this.pos.y - 30, 80, 60) ;
     image(this.image, this.pos.x, this.pos.y, 50, 50);
-    //image(img2, this.pos.x, this.pos.y, 50, 50);
-
-
 
   }
 
